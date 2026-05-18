7db865cba0added2466aaa6e017ddaa20111dc00
svirilinmax <mak.svirilin@gmail.com>
2026-05-18 14:39:54 +0300

    feat(db): setup database, models and alembic migrations

---
+++ app/models/weather.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
+from app.database import Base
+
+
+class WeatherRequest(Base):
+    __tablename__ = "weather_requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city = Column(String, index=True, nullable=False)
+    temperature = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    humidity = Column(Integer, nullable=False)
+    units = Column(String(1), nullable=False)
+    is_cached = Column(Boolean, default=False, nullable=False)
+
+    timestamp = Column(
+        DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+    )
